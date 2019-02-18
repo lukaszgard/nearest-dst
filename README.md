9835bd0ce6aa8873ff298df74eda294bc86aafda
Łukasz Gardoń <lukaszgardon555@gmail.com>
2019-02-19 00:40:05 +0100

    Update README.md (add Installation and Usage); and add one simple flag to store log in file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # nearest-dst
-This is web REST API application used to obtain and sort nearest/fastest destinations. 
+This is a simple REST API application used to obtain and sort nearest/fastest destinations.
+
+# Installation
+`
+$ go get github.com/lukaszgard/nearest-dst
+`
+
+# Usage
+`
+$ nearest-dst
+`
+
+- or with providing a path to log file:
+
+`
+$ nearest-dst -log-file=nearest.log
+`
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
 )
 
+var logFilename string
+
 func init() {
+	flag.StringVar(&logFilename, "log-file", "", "name of logfile")
+	flag.Parse()
+
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
 
-	log.SetOutput(os.Stdout)
+	if len(logFilename) > 0 {
+		f, err := os.OpenFile(logFilename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			log.SetOutput(os.Stdout)
+			log.Panic("Unable to open/create file: ", logFilename)
+		}
+		log.SetOutput(f)
+	} else {
+		log.SetOutput(os.Stdout)
+	}
 	log.SetLevel(log.TraceLevel)
 }
 
